0c8a4f96e292790f3f1551cc5e1e1ea20307e53e
IanGelfand <gelfandian@gmail.com>
2023-03-05 18:29:33 -0500

    Fix build errors

--- pages/components/DataTable.tsx
+++ pages/components/DataTable.tsx
@@ -20,82 +20,74 @@ function DataTable({
 	allChecked,
 	isIndeterminate,
 }: any) {
-	const handleCheckAll = (event: React.ChangeEvent<HTMLInputElement>) => {
+	const handleCheckAll = (event: any) => {
 		const { checked } = event.target;
 		setCheckedItems(new Array(data.length).fill(checked));
 	};
 
-	const handleCheckRow = (index: number, checked: boolean) => {
+	const handleCheckRow = (index: number, checked: any) => {
 		const newCheckedItems = checkedItems.map((item: any, i: number) =>
 			i === index ? checked : item
 		);
 		setCheckedItems(newCheckedItems);
 	};
 
-	const renderTableHeader = () => (
-		<Tr>
-			{columns.length &&
-				columns.map((column: any, index: number) => (
-					<Th key={index}>
-						{column.key === "id" ? (
-							<Flex align={"center"}>
-								<Checkbox
-									isChecked={allChecked}
-									isIndeterminate={isIndeterminate}
-									onChange={handleCheckAll}
-								/>
-								<Text ml={2}>{column.header}</Text>
-							</Flex>
-						) : (
-							<Text>{column.header}</Text>
-						)}
-					</Th>
-				))}
-		</Tr>
-	);
-
-	const renderTableRows = () => (
-		<>
-			{data.length &&
-				data.map((rowData: any, rowIndex: number) => (
-					<Tr key={rowIndex}>
-						{columns.map((column: any, columnIndex: number) => (
-							<Td key={columnIndex}>
-								{column.key === "id" ? (
-									<Flex align={"center"}>
-										<Checkbox
-											isChecked={checkedItems[rowIndex]}
-											onChange={(event) =>
-												handleCheckRow(rowIndex, event.target.checked)
-											}
-										/>
-										<Link
-											style={{
-												color: "#0081FF",
-												textDecoration: "underline",
-											}}
-											href={`/accounts/${rowData.id}`}
-										>
-											<Text ml={2} cursor={"pointer"}>
-												{rowData[column.key]}
-											</Text>
-										</Link>
-									</Flex>
-								) : (
-									<Text>{rowData[column.key]}</Text>
-								)}
-							</Td>
-						))}
-					</Tr>
-				))}
-		</>
-	);
-
 	return (
 		<TableContainer>
 			<Table>
-				<Thead bg={"gray.50"}>{renderTableHeader()}</Thead>
-				<Tbody>{renderTableRows()}</Tbody>
+				<Thead bg={"gray.50"}>
+					<Tr>
+						{columns?.map((column: any, index: number) => (
+							<Th key={index}>
+								{column.key === "id" ? (
+									<Flex align={"center"}>
+										<Checkbox
+											isChecked={allChecked}
+											isIndeterminate={isIndeterminate}
+											onChange={handleCheckAll}
+										/>
+										<Text ml={2}>{column.header}</Text>
+									</Flex>
+								) : (
+									<Text>{column.header}</Text>
+								)}
+							</Th>
+						))}
+					</Tr>
+				</Thead>
+				<Tbody>
+					{data?.map((rowData: any, rowIndex: number) => (
+						<Tr key={rowIndex}>
+							{columns?.map((column: any, columnIndex: number) => (
+								<Td key={columnIndex}>
+									{column.key === "id" ? (
+										<Flex align={"center"}>
+											<Checkbox
+												isChecked={checkedItems[rowIndex]}
+												onChange={(event) =>
+													handleCheckRow(rowIndex, event.target.checked)
+												}
+											/>
+											<Link
+												style={{
+													color: "#0081FF",
+													textDecoration: "underline",
+												}}
+												href={`/accounts/${rowData.id}`}
+											>
+												<Text ml={2} cursor={"pointer"}>
+													{rowData[column.key]}
+												</Text>
+											</Link>
+										</Flex>
+									) : (
+										<Text>{rowData[column.key]}</Text>
+									)}
+								</Td>
+							))}
+						</Tr>
+					))}
+				</Tbody>
 			</Table>
 		</TableContainer>
 	);
